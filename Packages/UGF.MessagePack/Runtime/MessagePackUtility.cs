95baa31d6417e32fd10389ac6595a4af50569941
Alexander <vorobyovvorobyov@gmail.com>
2019-05-16 19:03:52 +0300

    Remove all

--- Packages/UGF.MessagePack/Runtime/MessagePackUtility.cs
+++ Packages/UGF.MessagePack/Runtime/MessagePackUtility.cs
@@ -1,59 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using MessagePack;
-using MessagePack.Resolvers;
-using UGF.Assemblies.Runtime;
-using UGF.MessagePack.Runtime.Resolvers.Unity;
-using UGF.MessagePack.Runtime.Resolvers.Unity.Extension;
-using UGF.Types.Runtime;
-
-namespace UGF.MessagePack.Runtime
+﻿namespace UGF.MessagePack.Runtime
 {
     public static class MessagePackUtility
     {
-        public static IComparer<IFormatterResolver> FormatterResolverComparer { get; } = new MessagePackFormatterResolverComparer();
-
-        public static IFormatterResolver CreateDefaultResolver(Assembly assembly = null)
-        {
-            var resolver = new CompositeResolver();
-
-            resolver.RegisterResolver(CreateResolver(assembly));
-            resolver.RegisterResolver(UnityBlitWithPrimitiveArrayResolver.Instance);
-            resolver.RegisterResolver(UnityResolver.Instance);
-            resolver.RegisterResolver(BuiltinResolver.Instance);
-
-            return resolver;
-        }
-
-        public static IFormatterResolver CreateResolver(Assembly assembly = null)
-        {
-            var resolver = new CompositeResolver();
-            var resolvers = new List<IFormatterResolver>();
-
-            GetResolvers(resolvers, assembly);
-
-            resolvers.Sort(FormatterResolverComparer);
-
-            for (int i = 0; i < resolvers.Count; i++)
-            {
-                resolver.RegisterResolver(resolvers[i]);
-            }
-
-            return resolver;
-        }
-
-        public static void GetResolvers(ICollection<IFormatterResolver> resolvers, Assembly assembly = null)
-        {
-            if (resolvers == null) throw new ArgumentNullException(nameof(resolvers));
-
-            foreach (Type type in AssemblyUtility.GetBrowsableTypes<MessagePackFormatterResolverAttribute>(assembly))
-            {
-                if (TypesUtility.TryCreateType(type, out IFormatterResolver resolver))
-                {
-                    resolvers.Add(resolver);
-                }
-            }
-        }
     }
 }
